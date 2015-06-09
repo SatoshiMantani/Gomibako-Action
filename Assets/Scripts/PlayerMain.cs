18e4fc942c27f8d152007b337290658c64715566
SatoshiMantani <satoshimantani@gmail.com>
2015-06-09 20:58:08 +0900

    Animation不具合修正

--- Assets/Scripts/PlayerMain.cs
+++ Assets/Scripts/PlayerMain.cs
@@ -2,32 +2,29 @@
 using System.Collections;
 
 public class PlayerMain : MonoBehaviour {
-
-	//=====キャッシュ=====
-
-	PlayerController playerCtrl;
-
-	//========コード（Monobehaviour基本機能の実装）==========
-	void Awake(){
-
-		playerCtrl = GetComponent<PlayerController> ();
+	
+	// === キャッシュ ==========================================
+	PlayerController 	playerCtrl;
+	
+	// === コード（Monobehaviour基本機能の実装） ================
+	void Awake () {
+		playerCtrl = GetComponent<PlayerController>();
 	}
-
-
+	
 	void Update () {
-	//操作可能か？
+		// 操作可能か？
 		if (!playerCtrl.activeSts) {
 			return;
 		}
-
-	//パッド処理
+		
+		// パッド処理
 		float joyMv = Input.GetAxis ("Horizontal");
 		playerCtrl.ActionMove (joyMv);
-
-	//ジャンプ
+		
+		// ジャンプ
 		if (Input.GetButtonDown ("Jump")) {
-			playerCtrl.ActionJump();
+			playerCtrl.ActionJump ();
 		}
-
 	}
+	
 }
